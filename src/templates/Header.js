6af1badfa7cebb364cf15237f9ee8156204549f2
sebastian <luciosebastianboari@gmail.com>
2023-11-04 19:52:33 -0300

    feat: wickrick icon

--- src/templates/Header.js
+++ src/templates/Header.js
@@ -1,19 +1,36 @@
+import GithubIcon from '../icons/GithubIcon'
+import HomeIcon from '../icons/HomeIcon'
+import InfoIcon from '../icons/InfoIcon'
+import WickrickIcon from '../icons/WickrickIcon'
+// <img src="https://i.imgur.com/usvTZv0.png" alt="wikirick logo">
 const Header = () => {
     const view = `
-        <div class="Header-main">
-            <div class="Header-logo">
-                <h1>
-                    <a href="/">
-                        100tifi.com
-                    </a>
-                </h1>
-            </div>
-
-            <div class="Header-nav">
-                <a href="#/about/">
-                    About
+        <div class="header">
+            <div class="header_brand">
+                <a href="/" class="header_brand-link">
+                    ${WickrickIcon()}
                 </a>
             </div>
+
+            <ul class="header_nav">
+                <li class="header_nav-item">
+                    <a class="item_link" href="/">
+                        ${HomeIcon()}
+                    </a>
+                </li>
+                
+                <li class="header_nav-item">
+                    <a class="item_link" href="/">
+                        ${GithubIcon()}
+                    </a>
+                </li>
+
+                <li class="header_nav-item">
+                    <a class="item_link" href="#/about/">
+                        ${InfoIcon()}
+                    </a>
+                </li>
+            </ul>
         </div>
     `
     return view
