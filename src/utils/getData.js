752809c17f7fd00ac21f12d961a6221a0fa9527e
sebastian <luciosebastianboari@gmail.com>
2023-11-04 16:12:24 -0300

    feat: search only ricks

--- src/utils/getData.js
+++ src/utils/getData.js
@@ -1,13 +1,22 @@
-const API = 'https://rickandmortyapi.com/api/character/'
+const API = 'https://rickandmortyapi.com/api/character/?name=rick'
 
-const getData = async (id) => {
-    const apiURL = id ? `${API}${id}` : API
+const getData = async (id, page) => {
+    let apiURL
+
+    if (!id && !page) {
+        apiURL = API
+    } else if (id) {
+        apiURL = `https://rickandmortyapi.com/api/character/${id}`
+    } else if (page) {
+        apiURL = `${API}&page=${page}`
+    }
 
     try {
         const response = await fetch(apiURL, {
             method: 'GET'
         });
         const data = await response.json()
+
         return data
     } catch (error) {
         console.log('Fetch error:', error)
